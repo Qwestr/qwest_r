f8202c362ce967594fe1644712f52a1da24190b7
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-08-23 14:25:49 -0400

    Added initial welcome text and encounter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,87 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
+    // Welcome text
     println!("Welcome to qwest_r!");
+    println!("I'm terribly sorry, but I don't seem to know who you are :(");
+    println!("Let's start with a name.  What should I call you?");
+
+    // Enter your name
+    let mut name = String::new();
+    
+    io::stdin()
+        .read_line(&mut name)
+        .expect("I'm sorry, I didn't quite understand that.");
+
+    // Response text
+    println!("Hello {}!  Pleasure to meet you :)", name.trim());
+
+    // Question text
+    println!("So, are you ready to start your first adventure?");
+    loop {
+        // Present options
+        println!("(1) Yes (2) No");
+        
+        // Get answer
+        let mut answer = String::new();
+        
+        io::stdin()
+            .read_line(&mut answer)
+            .expect("I'm sorry, I didn't quite understand that.");
+
+        let answer: u32 = match answer.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // Determine response action
+        match answer {
+            1 => {
+                println!("Awesome!");
+                break;
+            },
+            2 => println!("Are you sure?"),
+            _ => println!("(I'm sorry, I didn't quite understand that."),
+        }
+    }
+
+    // First encounter
+    println!("Uh oh, you encounter an animated skeleton!  He wants to attack you!");
+    println!("What do you want to do?");
+    loop {
+        // Present options
+        println!("(1) Attack (2) Run");
+        
+        // Get answer
+        let mut answer = String::new();
+        
+        io::stdin()
+            .read_line(&mut answer)
+            .expect("I'm sorry, I didn't quite understand that.");
+
+        let answer: u32 = match answer.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // Determine response action
+        match answer {
+            1 => {
+                // Roll for attack
+                let attack_roll = rand::thread_rng().gen_range(1, 7);
+                println!("You attack with your cool sword for {} damage!", attack_roll);
+
+                // Roll for counter attack
+                let counter_attack_roll = rand::thread_rng().gen_range(1, 4);
+                println!("The skeleton attacks you for {} damage!", counter_attack_roll);
+
+            },
+            2 => {
+                println!("Bye!");
+                break;
+            },
+            _ => println!("(I'm sorry, I didn't quite understand that."),
+        }
+    }
 }
