7371c7b5ea47d766230235f5d20e1fbb81ad5206
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-09-09 00:28:50 -0400

    Added center/ intersects_with methods to Rect

--- src/main.rs
+++ src/main.rs
@@ -73,6 +73,21 @@ impl Rect {
             y2: y + h,
         }
     }
+
+    pub fn center(&self) -> (i32, i32) {
+        // Returns tuple containing (x, y) coords of Rect
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+    
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        // Returns true if this rectangle intersects with another one
+        (self.x1 <= other.x2)
+            && (self.x2 >= other.x1)
+            && (self.y1 <= other.y2)
+            && (self.y2 >= other.y1)
+    }
 }
 
 // A tile of the map and its properties
