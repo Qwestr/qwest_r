84d259278faa82b47151d0ebf4a8f19d5636a6cf
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-09-10 01:44:00 -0400

    Implemented ai_take_turn

--- src/main.rs
+++ src/main.rs
@@ -473,11 +473,11 @@ fn ai_take_turn(monster_id: usize, tcod: &Tcod, game: &Game, objects: &mut [Obje
     let (monster_x, monster_y) = objects[monster_id].pos();
     if tcod.fov.is_in_fov(monster_x, monster_y) {
         if objects[monster_id].distance_to(&objects[PLAYER]) >= 2.0 {
-            // move towards player if far away
+            // Move towards player if far away
             let (player_x, player_y) = objects[PLAYER].pos();
             move_towards(monster_id, player_x, player_y, &game.map, objects);
         } else if objects[PLAYER].fighter.map_or(false, |f| f.hp > 0) {
-            // close enough, attack! (if the player is still alive.)
+            // Close enough, attack! (if the player is still alive.)
             let monster = &objects[monster_id];
             println!(
                 "The attack of the {} bounces off your shiny metal armor!",
@@ -627,10 +627,9 @@ fn main() {
 
         // Let monsters take their turn
         if objects[PLAYER].alive && player_action != PlayerAction::DidntTakeTurn {
-            for object in &objects {
-                // only if object is not player
-                if (object as *const _) != (&objects[PLAYER] as *const _) {
-                    // println!("The {} growls!", object.name);
+            for id in 0..objects.len() {
+                if objects[id].ai.is_some() {
+                    ai_take_turn(id, &tcod, &game, &mut objects);
                 }
             }
         }
