430aa685aefe49090d5a773fafe90e41b5263ee7
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-09-10 01:55:34 -0400

    Created mut_two function

--- src/main.rs
+++ src/main.rs
@@ -244,6 +244,19 @@ struct Tcod {
     fov: FovMap,
 }
 
+// Mutably borrow two *separate* elements from the given slice.
+fn mut_two<T>(first_index: usize, second_index: usize, items: &mut [T]) -> (&mut T, &mut T) {
+    // Panic when the indexes are equal or out of bounds.
+    assert!(first_index != second_index);
+    let split_at_index = cmp::max(first_index, second_index);
+    let (first_slice, second_slice) = items.split_at_mut(split_at_index);
+    if first_index < second_index {
+        (&mut first_slice[first_index], &mut second_slice[0])
+    } else {
+        (&mut second_slice[0], &mut first_slice[second_index])
+    }
+}
+
 fn create_room(room: Rect, map: &mut Map) {
     // Go through the tiles in the rectangle and make them passable
     for x in (room.x1 + 1)..room.x2 {
