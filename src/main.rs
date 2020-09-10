aefc5b52bc9e9c39ca44e782887807ac357d3502
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-09-10 12:44:59 -0400

    Added Messages implementation

--- src/main.rs
+++ src/main.rs
@@ -114,6 +114,22 @@ struct Messages {
     messages: Vec<(String, Color)>,
 }
 
+impl Messages {
+    pub fn new() -> Self {
+        Self { messages: vec![] }
+    }
+
+    // Add the new message as a tuple, with the text and the color
+    pub fn add<T: Into<String>>(&mut self, message: T, color: Color) {
+        self.messages.push((message.into(), color));
+    }
+
+    // Create a `DoubleEndedIterator` over the messages
+    pub fn iter(&self) -> impl DoubleEndedIterator<Item = &(String, Color)> {
+        self.messages.iter()
+    }
+}
+
 // This is a generic object: the player, a monster, an item, the stairs...
 // It's always represented by a character on screen.
 #[derive(Debug)]
